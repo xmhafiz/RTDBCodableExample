7de1ac326fc135af203dfd4e99c7f557c31f5892
Mohd Hafiz <xmhafiz@gmail.com>
2021-10-03 07:08:24 +0800

    init project

---
+++ RTDBCodable/RTDBCodableApp.swift
@@ -0,0 +1,17 @@
+//
+//  RTDBCodableApp.swift
+//  RTDBCodable
+//
+//  Created by Hafiz on 03/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct RTDBCodableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
