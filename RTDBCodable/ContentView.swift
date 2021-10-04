10dfc3a8083237a2530110ff0dfda936a5bc4335
Mohd Hafiz <xmhafiz@gmail.com>
2021-10-04 12:07:15 +0800

    Added home view

--- RTDBCodable/ContentView.swift
+++ RTDBCodable/ContentView.swift
@@ -8,28 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @EnvironmentObject private var viewModel: TaskViewModel
-    
     var body: some View {
-        NavigationView {
-            VStack {
-                AddTaskView()
-                List {
-                    ForEach(viewModel.tasks, id: \.id) { task in
-                        TaskRowView(task: task)
-                    }
-                    .onDelete(perform: deleteRow)
-                }
-                .animation(.default)
-            }
-            .navigationBarTitle("Todo")
-        }
-    }
-    
-    private func deleteRow(indexSet: IndexSet) {
-        guard let index = indexSet.first else { return }
-        let taskId = viewModel.tasks[index].id
-        viewModel.remove(id: taskId)
+        HomeView()
     }
 }
 
---
+++ RTDBCodable/HomeView.swift
@@ -0,0 +1,45 @@
+//
+//  HomeView.swift
+//  RTDBCodable
+//
+//  Created by Hafiz on 04/10/2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject private var viewModel: TaskViewModel
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                AddTaskView()
+                List {
+                    ForEach(viewModel.tasks, id: \.id) { task in
+                        TaskRowView(task: task)
+                    }
+                    .onDelete(perform: deleteRow)
+                }
+                .animation(.default)
+            }
+            .navigationBarTitle("Todo")
+        }
+    }
+    
+    private func deleteRow(indexSet: IndexSet) {
+        guard let index = indexSet.first else { return }
+        let taskId = viewModel.tasks[index].id
+        viewModel.remove(id: taskId)
+    }
+}
+
+
+
+//        let id = UUID().uuidString
+//        ref.child("tasks/\(id)")
+//            .setValue([
+//                "id": id,
+//                "title": title,
+//                "completed": false,
+//                "updatedAt": Date().timeIntervalSince1970
+//            ])
